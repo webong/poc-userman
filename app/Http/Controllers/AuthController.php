9b72c36fdb2b340d77078348615b1898be44849c
Wisdom Ebong <wisdomaebong@gmail.com>
2024-12-14 17:15:40 +0100

    Set up a fresh Laravel app

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:8|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('UserToken')->accessToken;
+
+        return response()->json([
+            'message' => 'success',
+            'user' => $user,
+            'token' => $token,
+        ]);
+    }
+
+    
+}
